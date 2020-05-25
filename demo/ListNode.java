051b629a5adcdf711fb72bbb0e78ad8d49c03639
lhz <2190732276@qq.com>
2020-05-25 14:27:16 +0800

    两个中位数

---
+++ demo/ListNode.java
@@ -0,0 +1,14 @@
+package org.example.leetcode300.demo;
+
+/**
+ * @author: lhz
+ * @date: 2020/5/22
+ **/
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+}
---
+++ test/Pre10.java
@@ -0,0 +1,158 @@
+package org.example.leetcode300.test;
+
+import org.example.leetcode300.demo.ListNode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author: lhz
+ * @date: 2020/5/22
+ **/
+public class Pre10 {
+    public static void main(String[] args) {
+//        ListNode l1 = new ListNode(9);
+//        ListNode l2= new ListNode(9);
+//        ListNode l3 = new ListNode(9);
+//        l1.next = l2;
+//        l2.next = l3;
+//        ListNode n1 = new ListNode(9);
+//        ListNode n2= new ListNode(9);
+//        ListNode n3 = new ListNode(9);
+//        n1.next = n2;
+//        n2.next = n3;
+//        ListNode node = addTowNumber(l1, n1);
+//        System.out.println();
+
+
+        int[] num2 = {1,3};
+        int[] num1 = {4,5,6,7};
+        System.out.println(MedianofTwoSortedArrays(num1, num2));
+        String s = "abbdswq";
+        System.out.println(lengthOfLongestSubstring(s));
+    }
+
+    /**
+     * 两数相加
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public static ListNode addTowNumber(ListNode l1, ListNode l2) {
+        ListNode curr;
+        ListNode dummy = new ListNode(0);
+        curr = dummy;
+        ListNode p1 = l1;
+        ListNode p2 = l2;
+        int carry = 0;
+
+        while (p1 != null || p2 != null||carry>0) {
+            int x = p1 == null ?  0 :  p1.val;
+            int y = p2 == null ?  0 :  p2.val;
+            if (p1 != null) p1 = p1.next;
+            if (p2 != null) p2 = p2.next;
+            int sum = x + y+carry;
+            ListNode node;
+            if (sum >=10) {
+                carry = sum / 10;
+                sum=sum%10;
+                node = new ListNode(sum);
+            }else {
+                node = new ListNode(sum);
+                carry = 0;
+            }
+            curr.next = node;
+            curr = node;
+        }
+        return dummy.next;
+    }
+
+    /**
+     * 最长公共子串
+     * @param s
+     * @return
+     */
+    public static int lengthOfLongestSubstring(String s) {
+        int result = 1;
+        for (int i = 0; i <s.length()-1 ; i++) {
+            HashSet<Character> set = new HashSet<>();
+            set.add(s.charAt(i));
+            int len=1;
+            for (int j = i+1; j <s.length() ; j++) {
+                if (set.contains(s.charAt(j))) {
+                    break;
+                }else {
+                    set.add(s.charAt(j));
+                    len++;
+                }
+            }
+            result = Math.max(result, len);
+        }
+        return result;
+    }
+
+    /**
+     * 两个数组的中位数
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    public static double MedianofTwoSortedArrays(int[] nums1, int[] nums2){
+        int m = nums1.length;
+        int n = nums2.length;
+        //保存上一个值
+        int left = 0;
+        //保存当前值
+        int right = 0;
+        //num1的指针
+        int p1 = 0;
+        //num2的指针
+        int p2 = 0;
+        for (int i = 0; i <=(m+n)/2 ; i++) {
+            left = right;
+            //不可能出现p1>m和p2>n的情况 所以这样来做
+//            if (p1 < m && (p2 >= n || nums1[p1] < nums2[p2])) {
+//                right = nums1[p1];
+//                p1++;
+//            }else {
+//                right = nums2[p2];
+//                p2++;
+//            }
+            if (p1 < m || p2 < n) {
+                if (p1 >= m) {
+                    right = nums2[p2++];
+                } else if (p2 >= n) {
+                    right = nums1[p1++];
+                }else {
+                    if (nums1[p1] < nums2[p2]) {
+                        right = nums1[p1++];
+                    }else {
+                        right = nums2[p2++];
+                    }
+                }
+            }
+        }
+        if (((m + n) & 1 )== 0) {
+            return (left+right)/2.0;
+        }else {
+            return right;
+        }
+    }
+
+    /**
+     * 最长公共子串
+     * @param str1
+     * @param str2
+     * @return
+     */
+    public int mgetLCSLength(String str1, String str2) {
+
+
+    }
+
+
+
+
+
+
+}
