95a4d7d40b87e362ab08e17cad4b78ad48e2c3aa
lhz <2190732276@qq.com>
2020-05-27 19:06:32 +0800

    数字反转

--- test/Pre10.java
+++ test/Pre10.java
@@ -37,6 +37,7 @@ public class Pre10 {
         System.out.println(longestPalindrome("abcdbbfcba"));
 
         System.out.println(convert("ABC", 1));
+        System.out.println(reverse(5321));
     }
 
     /**
@@ -246,6 +247,35 @@ public class Pre10 {
             result.append(row);
         }
         return result.toString();
+    }
+
+    /**
+     * Reverse Integer
+     * 很简单，就是输入整数，输出它的倒置。
+     * @param x
+     * @return
+     */
+    public static int reverse(int x) {
+        int result = 0;
+        while (x != 0) {
+            int tmp = x % 10;
+            x/=10;
+            if (result > Integer.MAX_VALUE/10 ){ return 0;}
+
+            if (result < Integer.MIN_VALUE/10 ){ return 0;}
+
+            result = result * 10 + tmp;
+        }
+        return result;
+    }
+
+    /**
+     * 字符串转换整数 (atoi)
+     * @param str
+     * @return
+     */
+    public int myAtoi(String str) {
+        str.replace(" ", "");
     }
 
 
