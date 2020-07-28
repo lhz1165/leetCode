fad0b4c751fcd786280cf90c819f42437853bb6b
lhz <2190732276@qq.com>
2020-07-28 21:26:17 +0800

    双指针

--- org/example/leetcode300/specified/two_point/TwoPointTest01.java
+++ org/example/leetcode300/specified/two_point/TwoPointTest01.java
@@ -306,11 +306,23 @@ public class TwoPointTest01 {
      */
     public int triangleCount(int[] S) {
         // write your code here
+        int left = 0;
+        int right = S.length - 1;
+        int ans = 0;
         Arrays.sort(S);
-        int result = 0;
-        for (int i = 2; i <S.length ; i++) {
-
+        for (int i = 0; i < S.length; i++) {
+            left = 0;
+            right = i - 1;
+            while (left < right) {
+                if (S[left] + S[right] > S[i]) {
+                    ans = ans + (right - left);
+                    right --;
+                } else {
+                    left ++;
+                }
+            }
         }
+        return ans;
 
     }
 
