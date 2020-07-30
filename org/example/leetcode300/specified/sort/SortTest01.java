d10df16992099377a0bbf8ddfc67232415dbd84a
lhz <2190732276@qq.com>
2020-07-30 15:10:48 +0800

    quick sort

--- org/example/leetcode300/specified/sort/SortTest01.java
+++ org/example/leetcode300/specified/sort/SortTest01.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
  **/
 public class SortTest01 {
     public static void main(String[] args) {
-        int[] arr = {3, 4,5, 2, 1,8,10,0,2,3};
+        int[] arr = {1,2,5,3,4};
         quickSort(arr, 0, arr.length - 1);
         System.out.println(Arrays.toString(arr));
     }
--- org/example/leetcode300/specified/two_point/TwoPointTest02.java
+++ org/example/leetcode300/specified/two_point/TwoPointTest02.java
@@ -1,5 +1,7 @@
 package org.example.leetcode300.specified.two_point;
 
+import java.util.Arrays;
+
 /**
  * @author: lhz
  * @date: 2020/7/29
@@ -7,8 +9,12 @@ package org.example.leetcode300.specified.two_point;
 public class TwoPointTest02 {
     public static void main(String[] args) {
         TwoPointTest02 t = new TwoPointTest02();
-        int[] nums ={7,7,9,8,6,6,8,7,9,8,6,6};
-        t.partitionArray(nums, 10);
+        int[] nums ={2,0,2,2,1,2,2,1,2,0,0,0,1};
+        t.sortColors2(nums);
+        System.out.println(Arrays.toString(nums));
+//        int[] nums2 = {3, 2, 1,3,1, 3, 2, 1};
+//        t.partitionArray(nums2, 2);
+
     }
 
     /**
@@ -25,12 +31,17 @@ public class TwoPointTest02 {
         int start = 0;
         int end = nums.length - 1;
         while (start <= end) {
+            //end 如果等于k 那么会把所有比k小得找出来,(start左边的一段)，然后大于或等于k的另一段无序（start另一段，包含start）
+            //0---end小于， start----length大于等于
             while (start <= end&&nums[end] >= k) {
                 end--;
             }
+            //start 如果等于k 那么会把所有比k大的找出来,(start右边的一段，包含start)
+            // 0---end小于等于， start----length大于
             while (start <= end&&nums[start] < k) {
                 start++;
             }
+            //如果都等于k，那么狠混乱，start左边右边都有等于k的，0---end小于等于 start----length大于等于
             if (start <= end) {
                 int tmp = nums[start];
                 nums[start] = nums[end];
@@ -88,7 +99,91 @@ public class TwoPointTest02 {
             return quickSelect(nums, i, end, k - (i - start));
         }
         return nums[j + 1];
+    }
 
+
+    /**
+     * 按照红白蓝排序
+     * 我们可以使用整数 0，1 和 2 分别代表红，白，蓝。
+     *
+     * 输入 : [1, 0, 1, 2]
+     * 输出 : [0, 1, 1, 2]
+     *
+     * 思路1：先把小于1的挪到最左边，再把小于2的挪动一次 就好
+     * 这里如果 nums[start] <= 1 ，nums[end] > 1 那么会让所有比1大的2先放在一起 start作为2的头
+     * 这里如果 nums[start] < 1 ，nums[end] >= 1 那么会让所有所有比1小的0先放在一起 start作为1的头
+     *这里如果 nums[start] <= 1 ，nums[end] >= 1 那么会让所有的0先放在一起 end作为1的头
+     *
+     * 思路2：如果不用挪两次
+     * @param nums
+     */
+    public void sortColors(int[] nums) {
+        // write your code here
+        int start = 0;
+        int end = nums.length - 1;
+        //因为这里取start==1 ，所以就是找到右边所有【只大于】
+        while (start <= end) {
+            while (start <= end && nums[start] <= 1) {
+                start++;
+            }
+            while (start <= end && nums[end] > 1) {
+                end--;
+            }
+            if (start <= end) {
+                int tmp = nums[start];
+                nums[start] = nums[ end];
+                nums[end] = tmp;
+                start++;
+                end--;
+            }
+        }
+
+        int start2 = 0;
+        int end2 = start-1;
+        while (start2 <= end2) {
+            while (start2 <= end2 && nums[start2] < 1) {
+                start2++;
+            }
+            while (start2 <= end2 && nums[end2] >= 1) {
+                end2--;
+            }
+            if (start2 <= end2) {
+                int tmp = nums[start2];
+                nums[start2] = nums[ end2];
+                nums[end2] = tmp;
+                start2++;
+                end2--;
+            }
+        }
+    }
+
+    /**
+     *思路2：两个指针 l左边包括全是0 ，r右边全是2，中间就全是1
+     * 三指针维护
+     * @param nums
+     */
+    public void sortColors2(int[] nums) {
+        int l = 0;
+        int r = nums.length - 1;
+        int i = 0;
+        while (i < r) {
+            if (nums[i] == 0) {
+                swap(nums, l, i);
+                l++;
+                i++;
+            } else if (nums[i] == 1) {
+                i++;
+            }else {
+                swap(nums, i, r);
+                r--;
+            }
+        }
+    }
+
+    private void swap(int[] nums, int l, int i) {
+        int tmp = nums[l];
+        nums[l] = nums[i];
+        nums[i] = tmp;
     }
 
 }
