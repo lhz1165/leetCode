1e892c945e65e2bd9deb162268ca7e47d3fcf154
lhz <2190732276@qq.com>
2020-11-02 16:36:03 +0800

    2020 11周赛题目

--- org/algorithm/leetcode300/Test01.java
+++ org/algorithm/leetcode300/Test01.java
@@ -1,8 +1,8 @@
 package org.algorithm.leetcode300;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+import java.util.*;
 
 /**
  * @author: lhz
@@ -10,9 +10,81 @@ import java.util.List;
  **/
 public class Test01 {
     public static void main(String[] args) {
-        List<Integer> is = new ArrayList<>(Arrays.asList(1,2,3,4,5,6));
-        Integer[] iis = new Integer[is.size()];
-        is.toArray(iis);
+        Test01 test01 = new Test01();
+        test01.slowestKey(new int[]{12,23,36,46,62}, "spuda");
+        test01.checkArithmeticSubarrays(new int[]{4, 6, 5, 9, 3, 7}, new int[]{0, 0, 2}, new int[]{2, 3, 5});
+        test01.minimumEffortPath(new int[][]{{1,2,2},{3,8,2},{5,3,5}});
+
+    }
+
+    /**
+     * [12,23,36,46,62]
+     * "spuda"
+     * 1629. 按键持续时间最长的键
+     * @param releaseTimes
+     * @param keysPressed
+     * @return
+     */
+    public char slowestKey(int[] releaseTimes, String keysPressed) {
+        if (keysPressed.length() == 1) {
+            return keysPressed.charAt(0);
+        }
+        char[] chars = keysPressed.toCharArray();
+        int maxGap = releaseTimes[0];
+        int prevTimes = releaseTimes[0];
+        int index = 0;
+        for (int i = 1; i < releaseTimes.length; i++) {
+            int gap = releaseTimes[i] - prevTimes;
+            if ((gap > maxGap)||(gap == maxGap&& chars[i]>chars[index])) {
+                index = i;
+                maxGap = gap;
+            }
+            prevTimes = releaseTimes[i];
+        }
+        return chars[index];
+    }
+
+    /**
+     *
+     * 1630. 等差子数组
+     * 4,6,5,9,3,7]
+     * [0,0,2]
+     * [2,3,5]
+     */
+    public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
+        List<Boolean> results = new ArrayList<>();
+        int n = l.length;
+        for (int i = 0; i < n; i++) {
+            int start = l[i];
+            int end = r[i];
+            int[] current = Arrays.copyOfRange(nums, start, end+1);
+            Arrays.sort(current);
+            Boolean result = isArithmetic(current);
+            results.add(result);
+        }
+        return results;
+
+    }
+
+    public boolean isArithmetic(int[] nums) {
+        if (nums.length <= 2) {
+            return true;
+        }
+        int gap = nums[1] - nums[0];
+        for (int i = 2; i < nums.length; i++) {
+            if (nums[i] - nums[i - 1] != gap) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 1631. 最小体力消耗路径
+     *
+     */
+    public int minimumEffortPath(int[][] heights) {
+
 
     }
 }
--- org/algorithm/leetcode300/specified/bfs/BfsTest.java
+++ org/algorithm/leetcode300/specified/bfs/BfsTest.java
@@ -155,6 +155,34 @@ public class BfsTest {
         }
         return root;
     }
+    public static TreeNode deserialize2(String data) {
+        // write your code here
+        if (data.equals("{}")) {
+            return null;
+        }
+        String[] vals = data.substring(1, data.length() - 1).split(",");
+        ArrayList<TreeNode> queue = new ArrayList<>();
+        TreeNode root = new TreeNode(Integer.parseInt(vals[0]));
+        queue.add(root);
+        int index = 0;
+        boolean isLeftChild = true;
+        for (int i = 1; i < vals.length; i++) {
+            if (!vals[i].equals("#")) {
+                TreeNode node = new TreeNode(Integer.parseInt(vals[i]));
+                if (isLeftChild) {
+                    queue.get(index).left = node;
+                } else {
+                    queue.get(index).right = node;
+                }
+                queue.add(node);
+            }
+            if (!isLeftChild) {
+                index++;
+            }
+            isLeftChild = !isLeftChild;
+        }
+        return root;
+    }
 
 
 
