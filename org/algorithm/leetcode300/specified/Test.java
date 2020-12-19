575bce169e7cfc42669061f9184fd2351b10727e
lhz1165 <2190732276@qq.com>
2020-12-19 23:51:18 +0800

    everyDay

--- org/algorithm/leetcode300/specified/Test.java
+++ org/algorithm/leetcode300/specified/Test.java
@@ -3,20 +3,15 @@ package org.algorithm.leetcode300.specified;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * @author: lhz
  * @date: 2020/7/27
  **/
 public class Test {
-    public  static  int a = 2;
-
-    static {
-          a = 1;
-    }
-
     public static void main(String[] args) {
-        System.out.println(Test.a);
+
     }
 
 
