8077286b7bf34e89a4ce59d0b2afd971f0b09643
lhz <2190732276@qq.com>
2020-12-31 15:35:51 +0800

    everyday

--- org/algorithm/leetcode300/nomal/test/everyday/EveryDay05.java
+++ org/algorithm/leetcode300/nomal/test/everyday/EveryDay05.java
@@ -1,7 +1,8 @@
 package org.algorithm.leetcode300.nomal.test.everyday;
 
-import java.util.Arrays;
-import java.util.Stack;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * @author: lhz
@@ -10,35 +11,69 @@ import java.util.Stack;
 public class EveryDay05 {
     public static void main(String[] args) {
         EveryDay05 e = new EveryDay05();
-        System.out.println(e.lastStoneWeight(new int[]{2,2}));
+        //[[0,2],[1,3],[2,4],[3,5],[4,6]]
+        int[][] ints = {{0, 2}, {1, 3},{2,4},{3,5},{4,6}};
+        System.out.println(e.eraseOverlapIntervals(ints));
     }
-
     /**
-     * 1046. 最后一块石头的重量
+     * 435. 无重叠区间
+     * 如果重合，需要删除，删掉右边界最长的那一段
+     * 接着遍历
      */
-    public int lastStoneWeight(int[] stones) {
-        Stack<Integer> stack = new Stack<>();
-        Stack<Integer> helper = new Stack<>();
-        Arrays.sort(stones);
-        for (int stone : stones) {
-            stack.push(stone);
+    public int eraseOverlapIntervals(int[][] intervals) {
+        if (intervals.length == 0) {
+            return 0;
         }
-        while (!stack.isEmpty() && stack.size() != 1 ) {
-            Integer bigger = stack.pop();
-            Integer little = stack.pop();
-            Integer gap = bigger - little;
-            if (gap == 0) {
-                continue;
+        List<Pair> pairs = new ArrayList<>();
+        for (int[] interval : intervals) {
+            pairs.add(new Pair(interval[0], interval[1]));
+        }
+        pairs.sort((o1, o2) -> {
+            if (o1.start - o2.start == 0) {
+                return o1.end - o2.end;
+            }else {
+                return o1.start - o2.start;
             }
-            while (!stack.isEmpty() && stack.peek() > gap) {
-                helper.push(stack.pop());
-            }
-            stack.push(gap);
-            while (!helper.isEmpty()) {
-                stack.push(helper.pop());
+        });
+        Pair pair = pairs.get(0);
+        int res = 0;
+        int prevStart = pair.start;
+        int prevEnd = pair.end;
+        for (int i = 1; i < pairs.size(); i++) {
+            Pair current = pairs.get(i);
+            int curStart = current.start;
+            int curEnd = current.end;
+            if (curStart < prevEnd) {
+                if (curStart == prevStart && prevEnd == curEnd) {
+                    res++;
+                }else if (curStart == prevStart && prevEnd < curEnd){
+                    res++;
+                } else if (curStart > prevStart && prevEnd >= curEnd){
+                    res++;
+                    prevStart = curStart;
+                    prevEnd = curEnd;
+                }else if (curStart > prevStart && curEnd > prevEnd){
+                    res++;
+                }
+            }else {
+                prevStart = curStart;
+                prevEnd = curEnd;
             }
         }
-        return stack.isEmpty() ? 0: stack.pop();
+        return res;
+
+
     }
 
+
+
+}
+class Pair {
+    int start;
+    int end;
+
+    public Pair(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
 }
