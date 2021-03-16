1b2a7bd85a76d13eb75e5fa6cc73d674a18e41ef
lhz1165 <2190732276@qq.com>
2021-03-16 21:10:59 +0800

    两个数组中位数

--- org/algorithm/leetcode/specified/array/ArrayTest04.java
+++ org/algorithm/leetcode/specified/array/ArrayTest04.java
@@ -105,5 +105,38 @@ public class ArrayTest04 {
         nums[j] = tmp;
     }
 
+    public double findMedianSortedArrays(int A[], int B[]) {
+        int n = A.length + B.length;
+
+        if (n % 2 == 0) {
+            return (findKth(A, 0, B, 0, n / 2) + findKth(A, 0, B, 0, n / 2 + 1)) / 2.0;
+        }
+
+        return findKth(A, 0, B, 0, n / 2 + 1);
+    }
+
+    // 找到两个数组中的第k个数
+    public  int findKth(int[] A, int startOfA, int[] B, int startOfB, int k){
+        if (startOfA >= A.length) {
+            return B[startOfB + k - 1];
+        }
+        if (startOfB >= B.length) {
+            return A[startOfA + k - 1];
+        }
+
+        if (k == 1) {
+            return Math.min(A[startOfA], B[startOfB]);
+        }
+
+        int halfKthOfA = startOfA + (k / 2) - 1 < A.length ? A[startOfA + k / 2 - 1] : Integer.MAX_VALUE;
+        int halfKthOfB = startOfB + (k / 2) - 1 < B.length ? B[startOfB + k / 2 - 1] : Integer.MAX_VALUE;
+
+        if (halfKthOfA < halfKthOfB) {
+            return findKth(A, startOfA + k / 2, B, startOfB, k - k / 2);
+        } else {
+            return findKth(A, startOfA, B, startOfB + k / 2, k - k / 2);
+        }
+    }
+
 
 }
