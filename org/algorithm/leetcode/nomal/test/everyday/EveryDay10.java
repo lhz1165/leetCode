ea57e2363ba60edf1e067da0bfc0b1272c612c8b
lhz <2190732276@qq.com>
2021-04-29 18:53:02 +0800

    ...

--- org/algorithm/leetcode/nomal/test/everyday/EveryDay10.java
+++ org/algorithm/leetcode/nomal/test/everyday/EveryDay10.java
@@ -2,6 +2,8 @@ package org.algorithm.leetcode.nomal.test.everyday;
 
 import org.algorithm.leetcode.basic.TreeNode;
 
+import java.util.*;
+
 public class EveryDay10 {
     private TreeNode resNode;
 
@@ -26,5 +28,112 @@ public class EveryDay10 {
         inorder(node.right);
     }
 
+//    1011. 在 D 天内送达包裹的能力
+//    传送带上的包裹必须在 D 天内从一个港口运送到另一个港口。
+//
+//    传送带上的第 i 个包裹的重量为 weights[i]。
+//    每一天，我们都会按给出重量的顺序往传送带上装载包裹。我们装载的重量不会超过船的最大运载重量。
+//
+//    返回能在 D 天内将传送带上的所有包裹送达的船的最低运载能力。
+//    示例 1：
+//    输入：weights = [1,2,3,4,5,6,7,8,9,10], D = 5
+//    输出：15
+//    解释：
+//    船舶最低载重 15 就能够在 5 天内送达所有包裹，如下所示：
+//    第 1 天：1, 2, 3, 4, 5
+//    第 2 天：6, 7
+//    第 3 天：8
+//    第 4 天：9
+//    第 5 天：10
+//
+//    请注意，货物必须按照给定的顺序装运，因此使用载重能力为 14 的船舶并将包装分成 (2, 3, 4, 5), (1, 6, 7), (8), (9), (10) 是不允许的。
+//
+//    public int shipWithinDays(int[] weights, int D) {
+//
+//
+//    }
+
+    /**
+     * 403. 青蛙过河
+     * 输入：stones = [0,1,3,5,6,8,12,17]
+     * 输出：true
+     *
+     * 开始时， 青蛙默认已站在第一块石子上，并可以假定它第一步只能跳跃一个单位（即只能从单元格 1 跳至单元格 2 ）。
+     *
+     * 如果青蛙上一步跳跃了k个单位，那么它接下来的跳跃距离只能选择为k - 1、k 或 k + 1 个单位。 另请注意，青蛙只能向前方（终点的方向）跳跃。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/frog-jump
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     * 解释：青蛙可以成功过河，按照如下方案跳跃：
+     * 跳 1 个单位到第 2 块石子, 然后跳 2 个单位到第 3 块石子, 接着 跳 2 个单位到第 4 块石子, 然后跳 3 个单位到第 6 块石子, 跳 4 个单位到第 7 块石子, 最后，跳 5 个单位到第 8 个石子（即最后一块石子）。
+     */
+    public static void main(String[] args) {
+        EveryDay10 e = new EveryDay10();
+        boolean b = e.canCross(new int[]{0,1,2,3,4,8,9,11});
+        System.out.println(b);
+    }
+    public boolean canCross(int[] stones) {
+        if (stones.length == 0) {
+            return true;
+        }
+        int n = stones.length;
+        boolean[] f = new boolean[n];
+        Map<Integer, List<Integer>> index2Way = new HashMap<>();
+        int step = 1;
+        f[0] = true;
+        if (stones[1] - stones[0] == 1) {
+            f[1] = true;
+            index2Way.put(1,new ArrayList<>(Collections.singletonList(1)));
+        }else {
+            return false;
+        }
+        for (int i = 2; i < n; i++) {
+            f[i]  = false;
+            List<Integer> mem = new ArrayList<>();
+            for (int j = 1; j < i; j++) {
+                if (f[j]) {
+                    List<Integer> jWays = index2Way.get(j);
+                    if (jWays != null) {
+                        for (Integer jWay : jWays) {
+                            if (stones[i] - stones[j] == jWay) {
+                                f[i] = true;
+                                mem.add(jWay);
+                            } else if (stones[i] - stones[j] == jWay+1) {
+                                f[i] = true;
+                                mem.add(jWay+1);
+                            } else if (stones[i] - stones[j] == jWay - 1) {
+                                f[i] = true;
+                                mem.add(jWay - 1);
+                            }
+                        }
+                    }
+                }
+            }
+            index2Way.put(i, mem);
+        }
+        return f[n - 1];
+
+    }
+    public boolean canCross2(int[] stones) {
+        int n = stones.length;
+        //我们也可以使用动态规划的方法，令f[i][k]
+        // use k step can reach i
+        boolean[][] f = new boolean[n][n];
+        f[0][0] = true;
+        for (int i = 1; i < n; i++) {
+            f[i][0] = false;
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int step = stones[i] - stones[j];
+
+
+            }
+        }
+
+
+
+    }
 
 }
