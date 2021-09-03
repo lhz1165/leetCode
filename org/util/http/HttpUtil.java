01541e6de58570e277deb7c3fa48cd30815f2ec5
lhz <2190732276@qq.com>
2021-09-03 16:18:10 +0800

    http util

---
+++ org/util/http/HttpUtil.java
@@ -0,0 +1,38 @@
+package org.util.http;
+
+import org.util.http.client.BasicHttpClient;
+import org.util.http.method.ParameterMap;
+import org.util.http.response.HttpResponse;
+
+import java.util.Map;
+
+/**
+ * Date: 2021/8/18
+ * Description:
+ *
+ * @author hz.lai
+ */
+public class HttpUtil {
+
+    public static String doPost(String url, String path, Map<String, Object> params, String ctype,
+                                String accessKey,
+                                String accessSecret) {
+        BasicHttpClient httpClient = new BasicHttpClient(url);
+        ParameterMap paramMap = httpClient.newParams();
+        paramMap.putAll(params);
+        return _doPost(path, paramMap, ctype, accessKey, accessSecret, httpClient);
+    }
+
+    public static String _doPost(String path, ParameterMap paramMap, String ctype,
+                                 String accessKey,
+                                 String accessSecret,
+                                 BasicHttpClient httpClient) {
+        HttpResponse response = httpClient.post(path, paramMap, ctype, accessKey, accessSecret);
+        if (response == null) {
+            return null;
+        }
+        return response.getBodyString();
+    }
+
+
+}
---
+++ org/util/http/method/HttpRequest.java
@@ -0,0 +1,81 @@
+package org.util.http.method;
+
+/**
+ * Holds data for an HTTP request to be made with the attached HTTP client.
+ * 
+ * @author hz.lai
+ */
+public abstract class HttpRequest {
+    
+    public static final String URLENCODED = "application/x-www-form-urlencoded;charset=utf-8";
+    public static final String APPLICATION_JSON = "application/json";
+    public static final String MULTIPART = "multipart/form-data";
+    
+    protected String path = ""; // avoid null in URL
+    protected HttpMethod httpMethod;
+    protected String contentType;
+    protected byte[] content;
+    protected String contentString;
+    protected String accessSecret;
+    protected String accessKey;
+
+    
+    /**
+     * Constructs a request with optional params appended
+     * to the query string.
+     * 
+     * @param path
+     * @param params
+     */
+    public HttpRequest(String path,String accessKey,String accessSecret) {
+        this.accessKey = accessKey;
+        this.accessSecret = accessSecret;
+        this.path = path;
+    }
+    public HttpRequest(String path, ParameterMap params) {
+        //拼接url
+//        if (URLENCODED.equals(contentType)) {
+//            String queryString = null;
+//            if (path != null) {
+//                this.path = path;
+//            }
+//            if (params != null) {
+//                queryString = params.urlEncode();
+//                this.path += "?" + queryString;
+//            }
+//        }
+        this.path = path;
+    }
+
+    public String getPath() {
+        return path;
+    }
+    
+    public HttpMethod getHttpMethod() {
+        return httpMethod;
+    }
+    
+    public String getContentType() {
+        return contentType;
+    }
+    
+    public byte[] getContent() {
+        return content;
+    }
+
+    public String getAccessSecret() {
+        return accessSecret;
+    }
+
+    public void setAccessSecret(String accessSecret) {
+        this.accessSecret = accessSecret;
+    }
+
+    public String getAccessKey() {
+        return accessKey;
+    }
+
+    public void setAccessKey(String accessKey) {
+        this.accessKey = accessKey;
+    }
+}
